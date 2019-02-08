a27dc0ca70773e8b12c3a472d8f4e5ffb16c94a3
rbairwell <rbairwell@users.noreply.github.com>
2019-02-08 15:25:30 +0000

    Add logs/fix issue with logs

--- src/CraftSilktide.php
+++ src/CraftSilktide.php
@@ -20,12 +20,13 @@ use silktide\craftsilktide\models\Settings;
 use yii\base\Event;
 
 /**
- * Craft plugins are very much like little applications in and of themselves. We’ve made
- * it as simple as we can, but the training wheels are off. A little prior knowledge is
- * going to be required to write a plugin.
+ * Craft plugins are very much like little applications in and of themselves.
+ * We’ve made it as simple as we can, but the training wheels are off. A little
+ * prior knowledge is going to be required to write a plugin.
  *
- * For the purposes of the plugin docs, we’re going to assume that you know PHP and SQL,
- * as well as some semi-advanced concepts like object-oriented programming and PHP namespaces.
+ * For the purposes of the plugin docs, we’re going to assume that you know PHP
+ * and SQL, as well as some semi-advanced concepts like object-oriented
+ * programming and PHP namespaces.
  *
  * https://craftcms.com/docs/plugins/introduction
  *
@@ -36,12 +37,13 @@ use yii\base\Event;
  */
 class CraftSilktide extends Plugin
 {
+
     // Static Properties
     // =========================================================================
 
     /**
-     * Static property that is an instance of this plugin class so that it can be accessed via
-     * CraftSilktide::$plugin
+     * Static property that is an instance of this plugin class so that it can
+     * be accessed via CraftSilktide::$plugin
      *
      * @var craftSilktide
      */
@@ -51,24 +53,25 @@ class CraftSilktide extends Plugin
     // =========================================================================
 
     /**
-     * To execute your plugin’s migrations, you’ll need to increase its schema version.
+     * To execute your plugin’s migrations, you’ll need to increase its schema
+     * version.
      *
      * @var string
      */
-    public $schemaVersion = '1.0.0';
+    public $schemaVersion = '1.0.1';
 
     // Public Methods
     // =========================================================================
 
     /**
-     * Set our $plugin static property to this class so that it can be accessed via
-     * craftsilktide::$plugin
+     * Set our $plugin static property to this class so that it can be accessed
+     * via craftsilktide::$plugin
      *
-     * Called after the plugin class is instantiated; do any one-time initialization
-     * here such as hooks and events.
+     * Called after the plugin class is instantiated; do any one-time
+     * initialization here such as hooks and events.
      *
-     * If you have a '/vendor/autoload.php' file, it will be loaded for you automatically;
-     * you do not need to load it in your init() method.
+     * If you have a '/vendor/autoload.php' file, it will be loaded for you
+     * automatically; you do not need to load it in your init() method.
      *
      */
     public function init()
@@ -87,12 +90,12 @@ class CraftSilktide extends Plugin
         $apiKey = $this->getSettings()->apiKey;
         if ($apiKey === '') {
             Craft::info(
-                Craft::t(
-                    'craft-silktide',
-                    '{name} plugin loaded, but not configured',
-                    ['name' => $this->name]
-                ),
-                __METHOD__
+              Craft::t(
+                'craft-silktide',
+                '{name} plugin loaded, but not configured',
+                ['name' => $this->name]
+              ),
+              __METHOD__
             );
             return;
         }
@@ -100,15 +103,6 @@ class CraftSilktide extends Plugin
         $this->initSaveEntry($apiKey);
 
         $this->initMetaTag($apiKey);
-
-        Craft::info(
-            Craft::t(
-                'craft-silktide',
-                '{name} plugin loaded',
-                ['name' => $this->name]
-            ),
-            __METHOD__
-        );
     }
 
     // Protected Methods
@@ -116,55 +110,59 @@ class CraftSilktide extends Plugin
 
     /**
      * Setup our save event.
+     *
      * @param string $apiKey
      */
     protected function initSaveEntry(string $apiKey)
     {
-        Event::on(Entry::class, Entry::EVENT_AFTER_SAVE, function (Event $e) use ($apiKey) {
-            /* @var Entry $sender */
-            $sender = $e->sender;
-            if ($sender->status === Entry::STATUS_LIVE) {
-                /**
-                 * If we have an entry which has been saved and is 'live', send notification to silktide.
-                 */
-                Craft::$app->getQueue()->push(new CraftSilktideJob([
+        Event::on(Entry::class, Entry::EVENT_AFTER_SAVE,
+          function (Event $e) use ($apiKey) {
+              /* @var Entry $sender */
+              $sender = $e->sender;
+              if ($sender->status === Entry::STATUS_LIVE) {
+                  /**
+                   * If we have an entry which has been saved and is 'live', send notification to silktide.
+                   */
+                  Craft::$app->getQueue()->push(new CraftSilktideJob([
                     'apiKey' => $apiKey,
-                    'urls' => $sender->url,
+                    'urls' => [$sender->url],
                     'userAgent' => 'SilktideCraft/' .
-                        $this->getVersion() .
-                        ' (compatible; CraftCMS ' .
-                        Craft::$app->getVersion() .
-                        ')'
-                ]));
-            }
-        });
+                      $this->getVersion() .
+                      ' (compatible; CraftCMS ' .
+                      Craft::$app->getVersion() .
+                      ')',
+                  ]));
+              }
+          });
     }
 
     /**
      * Setup the meta tag event.
+     *
      * @param string $apiKey
      */
     protected function initMetaTag(string $apiKey)
     {
-        Event::on(View::class, View::EVENT_BEFORE_RENDER_PAGE_TEMPLATE, function (TemplateEvent $e) use ($apiKey) {
-            // make sure we are public view only
-            if (Craft::$app->request->getIsSiteRequest() &&
+        Event::on(View::class, View::EVENT_BEFORE_RENDER_PAGE_TEMPLATE,
+          function (TemplateEvent $e) use ($apiKey) {
+              // make sure we are public view only
+              if (Craft::$app->request->getIsSiteRequest() &&
                 !Craft::$app->request->getIsCpRequest() &&
                 !Craft::$app->request->getIsConsoleRequest() &&
                 !(Craft::$app->request->hasMethod('getIsAjax') && Craft::$app->request->getIsAjax()) &&
                 !(Craft::$app->request->hasMethod('getIsLivePreview') && Craft::$app->request->getIsLivePreview())
-            ) {
-                // get current page element
-                $element = Craft::$app->urlManager->getMatchedElement();
-                if (!empty($element)) {
-                    $editLink = $element->getCpEditUrl();
-                    if ($editLink !== null) {
-                        $this->createMetaTag($apiKey, $editLink);
-                    }
-                }
-            }
+              ) {
+                  // get current page element
+                  $element = Craft::$app->urlManager->getMatchedElement();
+                  if (!empty($element)) {
+                      $editLink = $element->getCpEditUrl();
+                      if ($editLink !== null) {
+                          $this->createMetaTag($apiKey, $editLink);
+                      }
+                  }
+              }
 
-        });
+          });
     }
 
     /**
@@ -181,24 +179,24 @@ class CraftSilktide extends Plugin
             return;
         }
         $ciphertext_raw = openssl_encrypt(
-            json_encode(
-                [
-                    'editorUrl' => $editLink
-                ]
-            ),
-            'AES-256-CBC',
-            $apiKey,
-            OPENSSL_RAW_DATA,
-            $iv
+          json_encode(
+            [
+              'editorUrl' => $editLink,
+            ]
+          ),
+          'AES-256-CBC',
+          $apiKey,
+          OPENSSL_RAW_DATA,
+          $iv
         );
 
         $hmac = hash_hmac('sha256', $ciphertext_raw, $apiKey, true);
         $ciphertext = base64_encode($iv . $hmac . $ciphertext_raw);
         Craft::$app->getView()->registerMetaTag(
-            [
-                'name' => 'silktide-cms',
-                'content' => htmlspecialchars($ciphertext, ENT_QUOTES)
-            ]
+          [
+            'name' => 'silktide-cms',
+            'content' => htmlspecialchars($ciphertext, ENT_QUOTES),
+          ]
         );
     }
 
@@ -213,8 +211,8 @@ class CraftSilktide extends Plugin
     }
 
     /**
-     * Returns the rendered settings HTML, which will be inserted into the content
-     * block on the settings page.
+     * Returns the rendered settings HTML, which will be inserted into the
+     * content block on the settings page.
      *
      * @return string The rendered settings HTML
      *
@@ -224,10 +222,10 @@ class CraftSilktide extends Plugin
     protected function settingsHtml(): string
     {
         return Craft::$app->view->renderTemplate(
-            'craft-silktide/_settings',
-            [
-                'settings' => $this->getSettings()
-            ]
+          'craft-silktide/_settings',
+          [
+            'settings' => $this->getSettings(),
+          ]
         );
     }
 
--- src/CraftSilktideJob.php
+++ src/CraftSilktideJob.php
@@ -11,6 +11,7 @@
 namespace silktide\craftsilktide;
 
 use Craft;
+use craft\helpers\FileHelper;
 use craft\queue\BaseJob;
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\RequestOptions;
@@ -18,10 +19,12 @@ use Psr\Http\Message\ResponseInterface;
 
 /**
  * Class CraftSilktideJob
+ *
  * @package silktide\craftsilktide
  */
 class CraftSilktideJob extends BaseJob
 {
+
     const SILKTIDE_API_URL = 'https://api.silktide.com/cms/update';
 
     /**
@@ -53,49 +56,54 @@ class CraftSilktideJob extends BaseJob
     public function execute($queue)
     {
         Craft::info(
-            Craft::t(
-                'craft-silktide',
-                'Notifying Silktide about a change to URLs {urls}',
-                [
-                    'urls' => implode($this->urls, ', ')
-                ]
-            ),
-            __METHOD__
+          Craft::t(
+            'craft-silktide',
+            'Notifying Silktide about a change to URLs {urls}',
+            [
+              'urls' => implode(', ', $this->urls),
+            ]
+          ),
+          __METHOD__
         );
         $options = [
-            RequestOptions::BODY => http_build_query([
-                'apiKey' => $this->apiKey,
-                'urls' => [
-                    $this->urls
-                ]
-            ])
+          RequestOptions::BODY => http_build_query([
+            'apiKey' => $this->apiKey,
+            'urls' => [
+              $this->urls,
+            ],
+          ]),
         ];
         $client = Craft::createGuzzleClient(
-            [
-                'headers' => [
-                    'User-Agent' => $this->userAgent
-                ]
-            ]
+          [
+            'headers' => [
+              'User-Agent' => $this->userAgent,
+            ],
+          ]
         );
-
         try {
-            $response = $client->request('post', self::SILKTIDE_API_URL, $options);
+            $response = $client->request('post', self::SILKTIDE_API_URL,
+              $options);
             $this->handleValidResponse($response);
         } catch (RequestException $e) {
             $message = Craft::t(
-                'craft-silktide',
-                'Failed to notify Silktide - exception type {exception} message: {message}',
-                [
-                    'exception' => get_class($e),
-                    'message' => $e->getMessage()
-                ]
+              'craft-silktide',
+              'Failed to notify Silktide - exception type {exception} message: {message}',
+              [
+                'exception' => get_class($e),
+                'message' => $e->getMessage(),
+              ]
             );
             Craft::warning($message, __METHOD__);
+            $file = Craft::getAlias('@storage/logs/silktide.log');
+            $log = date('Y-m-d H:i:s') . ' ' . $message . PHP_EOL;
+            FileHelper::writeToFile($file, $log, ['append' => true]);
         }
     }
 
     /**
-     * @param ResponseInterface $response
+     * @param \Psr\Http\Message\ResponseInterface $response
+     *
+     * @throws \yii\base\ErrorException
      */
     protected function handleValidResponse(ResponseInterface $response)
     {
@@ -104,27 +112,29 @@ class CraftSilktideJob extends BaseJob
         if ($responseCode >= 200 && $responseCode <= 299) {
             $decoded = json_decode($body, true);
             if (is_array($decoded) && isset($decoded['status']) && $decoded['status'] === 'ok') {
-                Craft::info(
-                    Craft::t(
-                        'craft-silktide',
-                        'Notified Silktide and received a {status} code back with {body}',
-                        ['status' => $responseCode,
-                            'body' => $body
-                        ]
-                    ),
-                    __METHOD__
+                $message = Craft::t(
+                  'craft-silktide',
+                  'Notified Silktide and received a {status} code back with {body}',
+                  [
+                    'status' => $responseCode,
+                    'body' => $body,
+                  ]
                 );
+                Craft::info($message, __METHOD__);
             }
         }
         $message = Craft::t(
-            'craft-silktide',
-            'Failed to notify Silktide - HTTP request failed with status {status} and body {body}',
-            [
-                'status' => $responseCode,
-                'body' => $body
-            ]
+          'craft-silktide',
+          'Failed to notify Silktide - HTTP request failed with status {status} and body {body}',
+          [
+            'status' => $responseCode,
+            'body' => $body,
+          ]
         );
         Craft::warning($message, __METHOD__);
+        $file = Craft::getAlias('@storage/logs/silktide.log');
+        $log = date('Y-m-d H:i:s') . ' ' . $message . PHP_EOL;
+        FileHelper::writeToFile($file, $log, ['append' => true]);
     }
 
 }
