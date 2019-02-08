3e9ccae8c8006df4430dffc0c84fb6fb360242de
rbairwell <rbairwell@users.noreply.github.com>
2019-02-08 16:49:21 +0000

    Better error handling

--- src/CraftSilktideJob.php
+++ src/CraftSilktideJob.php
@@ -27,6 +27,8 @@ class CraftSilktideJob extends BaseJob
 
     const SILKTIDE_API_URL = 'https://api.silktide.com/cms/update';
 
+    //const SILKTIDE_API_URL = 'https://rbox.app/box/request/12cee683-394c-468e-a426-dc9219bf3c18';
+
     /**
      * @var string The API key we are using.
      */
@@ -67,15 +69,14 @@ class CraftSilktideJob extends BaseJob
         );
         $requestBody = http_build_query([
           'apiKey' => $this->apiKey,
-          'urls' => [
-            $this->urls,
-          ],
+          'urls' => $this->urls,
         ]);
         $options = [RequestOptions::BODY => $requestBody];
         $client = Craft::createGuzzleClient(
           [
             'headers' => [
               'User-Agent' => $this->userAgent,
+              'Content-Type' => 'application/x-www-form-urlencoded',
             ],
           ]
         );
@@ -112,9 +113,18 @@ class CraftSilktideJob extends BaseJob
     ) {
         $responseCode = (int)$response->getStatusCode();
         $body = $response->getBody()->getContents();
-        if ($responseCode >= 200 && $responseCode <= 299) {
+        $failReason = 'Unknown';
+        if ($responseCode < 200 && $responseCode > 299) {
+            $failReason = 'Status code';
+        } else {
             $decoded = json_decode($body, true);
-            if (is_array($decoded) && isset($decoded['status']) && $decoded['status'] === 'ok') {
+            if (!is_array($decoded)) {
+                $failReason = 'Unable to decode';
+            } elseif (!isset($decoded['status'])) {
+                $failReason = 'No status field';
+            } elseif ($decoded['status'] !== 'ok') {
+                $failReason = 'Status not ok';
+            } else {
                 $message = Craft::t(
                   'craft-silktide',
                   'Notified Silktide and received a {status} code back with {body} from {request}',
@@ -125,12 +135,17 @@ class CraftSilktideJob extends BaseJob
                   ]
                 );
                 Craft::info($message, __METHOD__);
+                $file = Craft::getAlias('@storage/logs/silktide.log');
+                $log = date('Y-m-d H:i:s') . ' ' . $message . PHP_EOL;
+                FileHelper::writeToFile($file, $log, ['append' => true]);
+                return;
             }
         }
         $message = Craft::t(
           'craft-silktide',
-          'Failed to notify Silktide - HTTP request failed with status {status} and body {body} from {request}',
+          'Failed to notify Silktide - HTTP request failed: ({reason}) with status {status} and body "{body}" from {request}',
           [
+            'reason' => $failReason,
             'status' => $responseCode,
             'body' => $body,
             'request' => $requestBody,
